ab1f1c51231c98b66ca2be7e0e041977060939ed
Axel Waldenborg <axel.waldenborg@learnet.se>
2024-09-13 09:42:50 +0200

    juice som fan

--- main.py
+++ main.py
@@ -2,24 +2,27 @@
 import random
 import os
 
+from playsound import playsound
+
 
 
   
 
 
+player = 0
+house =  0
+isRunning = True
 
-def main():
-    player = 0
-    house =  0
-    isRunning = True
 
-    while isRunning:
+
+while isRunning:
+
         print(isRunning)   
+        # playsound("sounds/dice_roll.mp3")
         player += random.randint(1,6)
         house += random.randint(1,6)
         print(f"Player has {player}")
         print(f"House has {house}")
-
 
         while True:
             if player>13:
@@ -29,6 +32,8 @@ def main():
                 
             print(f"You have {player}, do you want to stay Y/N")
             x = input()
+         
+
             if x.lower() == "y":
                 print("yes")
                 break
@@ -36,17 +41,21 @@ def main():
                 print("no")
                 player += random.randint(1,6)
 
-        while house < player:
-            house += random.randint(1,6)
+        if player <= 13:
+            while house < player:
+                house += random.randint(1,6)
 
-            if house > 13:
-                print("house has over 13, you win")
-                isRunning = False
-                break
+                if house > 13 :
+                            
+                    print("house has over 13, you win")
+                    playsound("sounds/yippie.mp3")
+                    isRunning = False
+                    break
+            if house <= 13:
+                print(f"House has {house}, house wins")
+                playsound("sounds/trombone.mp3")
 
-        print(f"House has {house}, house wins")
+        
         isRunning = False
 
     
-if __name__ == "__main__":
-    main()
